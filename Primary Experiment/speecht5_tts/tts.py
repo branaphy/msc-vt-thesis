b371b57ca2ad55ee4d63ee929952eb58d841126c
B. N <61725592+branaphy@users.noreply.github.com>
2024-06-08 22:26:27 +0200

    Update tts.py

--- Primary Experiment/speecht5_tts/tts.py
+++ Primary Experiment/speecht5_tts/tts.py
@@ -1,1 +1,15 @@
 # Code used to generate the audio files for the Microsoft SpeechT5 voice for the primary listening test.
+from transformers import pipeline
+from datasets import load_dataset
+import soundfile as sf
+import torch
+import torchvision
+
+synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts")
+
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+
+speech = synthesiser("TEXT PROMPT HERE", forward_params={"speaker_embeddings": speaker_embedding})
+
+sf.write("speecht5_multisent.wav", speech["audio"], samplerate=speech["sampling_rate"])
