8e964fa1bbdf8712c9635e50e8bd06afa82a7ca2
B. N <61725592+branaphy@users.noreply.github.com>
2024-06-08 22:36:45 +0200

    Update tts.py

--- Primary Experiment/parler-tts-mini-expresso/Jerry/tts.py
+++ Primary Experiment/parler-tts-mini-expresso/Jerry/tts.py
@@ -1,1 +1,22 @@
 # Code used to generate the audio clips for the parler-tts-mini-expresso voice, Jerry in the primary listening test.
+
+import torch
+from parler_tts import ParlerTTSForConditionalGeneration
+from transformers import AutoTokenizer, set_seed
+import soundfile as sf
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+model = ParlerTTSForConditionalGeneration.from_pretrained("parler-tts/parler-tts-mini-expresso").to(device)
+tokenizer = AutoTokenizer.from_pretrained("parler-tts/parler-tts-mini-expresso")
+
+prompt = "TEXT PROMPT HERE"
+description = "Jerry speaks moderately slowly in a default tone with high quality audio."
+
+input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
+prompt_input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+
+set_seed(42)
+generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
+audio_arr = generation.cpu().numpy().squeeze()
+sf.write("output.wav", audio_arr, model.config.sampling_rate)
